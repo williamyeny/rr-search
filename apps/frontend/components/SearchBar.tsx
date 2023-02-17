66e8cedcb295a829a11869a301d49fc9eab537b3
Will Ye <williamyeny@gmail.com>
2023-02-17 11:39:47 -0500

    Clear query button

--- apps/frontend/components/SearchBar.tsx
+++ apps/frontend/components/SearchBar.tsx
@@ -1,5 +1,6 @@
-import { SearchIcon } from "@chakra-ui/icons";
-import { Flex, Input, Button } from "@chakra-ui/react";
+import { CloseIcon, SearchIcon } from "@chakra-ui/icons";
+import { Flex, Input, Button, Box, IconButton } from "@chakra-ui/react";
+import { useRef } from "react";
 
 export const SearchBar = ({
   query,
@@ -11,33 +12,57 @@ export const SearchBar = ({
   setQuery: (query: string) => void;
   onSearch: () => void;
   isLoading?: boolean;
-}) => (
-  <form
-    onSubmit={(e) => {
-      e.preventDefault();
-      onSearch();
-    }}
-  >
-    <Flex gap={2} justify="space-between">
-      <Input
-        placeholder="Query"
-        borderRadius={50}
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-        backgroundColor="white"
-      />
-      <Button
-        right={0}
-        leftIcon={<SearchIcon />}
-        flexShrink={0}
-        backgroundColor="black"
-        color="white"
-        borderRadius={50}
-        isDisabled={isLoading}
-        type="submit"
-      >
-        Search
-      </Button>
-    </Flex>
-  </form>
-);
+}) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        onSearch();
+      }}
+    >
+      <Flex gap={2} justify="space-between">
+        <Box position="relative" w="100%">
+          <Input
+            ref={inputRef}
+            placeholder="Query"
+            borderRadius={50}
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            backgroundColor="white"
+          />
+          {query && (
+            <IconButton
+              borderRadius={50}
+              background="none"
+              _hover={{ background: "none" }}
+              icon={<CloseIcon boxSize="0.7em" />}
+              aria-label="Clear query"
+              position="absolute"
+              right={0}
+              onClick={() => {
+                setQuery("");
+                if (inputRef.current) inputRef.current.focus();
+              }}
+              zIndex={1} // TODO: use portal?
+            />
+          )}
+        </Box>
+
+        <Button
+          right={0}
+          leftIcon={<SearchIcon />}
+          flexShrink={0}
+          backgroundColor="black"
+          color="white"
+          borderRadius={50}
+          isDisabled={isLoading}
+          type="submit"
+        >
+          Search
+        </Button>
+      </Flex>
+    </form>
+  );
+};
